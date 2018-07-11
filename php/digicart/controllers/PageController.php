c84a298dbd039b9bf70e7ce2e611aa446f4c8122
learnWithUjjwal <ujwalgoyal.2@gmail.com>
2018-07-11 10:45:21 +0530

    Latest commit

--- php/digicart/controllers/PageController.php
+++ php/digicart/controllers/PageController.php
@@ -2,7 +2,7 @@
 require_once('AppController.php');
 class PageController extends AppController{
 	public function index(){
-		return parent::view('layouts/master.view.php', ['dc_content' => 'Content']);
+		return parent::view('layouts/master.view.php', ['dc_content' => 'Home Page']);
 	}
 	public function about(){
 		return parent::view('layouts/master.view.php', ['dc_content' => 'About']);
